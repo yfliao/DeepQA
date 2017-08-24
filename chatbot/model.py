7bb05b3a152e977eb426e66613652c72c68f6446
yfliao <yfliao@ntut.edu.tw>
2017-08-24 12:47:52 +0800

    test

--- chatbot/model.py
+++ chatbot/model.py
@@ -126,7 +126,6 @@ class Model:
                 # We need to compute the sampled_softmax_loss using 32bit floats to
                 # avoid numerical instabilities.
                 localWt     = tf.cast(outputProjection.W_t,             tf.float32)
-#                localWt     = tf.cast(outputProjection.W,             tf.float32)
                 localB      = tf.cast(outputProjection.b,               tf.float32)
                 localInputs = tf.cast(inputs,                           tf.float32)
 
@@ -136,8 +135,6 @@ class Model:
                         localB,
                         labels,
                         localInputs,
-#                        localInputs,
-#                        labels,
                         self.args.softmaxSamples,  # The number of classes to randomly sample per batch
                         self.textData.getVocabularySize()),  # The number of classes
                     self.dtype)
@@ -156,8 +153,36 @@ class Model:
                 )
             return encoDecoCell
 
+        # Creation of the rnn cell
+        def create_bi_rnn_cell():
+#            encoDecoCell = tf.contrib.rnn.BasicLSTMCell(  # Or GRUCell, LSTMCell(args.hiddenSize)
+            encoDecoCellFw = tf.contrib.rnn.LSTMCell(  # Or GRUCell, LSTMCell(args.hiddenSize)
+                self.args.hiddenSize,
+            )
+            encoDecoCellBw = tf.contrib.rnn.LSTMCell(  # Or GRUCell, LSTMCell(args.hiddenSize)
+                self.args.hiddenSize,
+            )
+            if not self.args.test:  # TODO: Should use a placeholder instead
+                encoDecoCellFw = tf.contrib.rnn.DropoutWrapper(
+                    encoDecoCellFw,
+                    input_keep_prob=1.0,
+                    output_keep_prob=self.args.dropout
+                )
+            if not self.args.test:  # TODO: Should use a placeholder instead
+                encoDecoCellBw = tf.contrib.rnn.DropoutWrapper(
+                    encoDecoCellBw,
+                    input_keep_prob=1.0,
+                    output_keep_prob=self.args.dropout
+                )
+            encoDecoCell = tf.nn.bidirectional_dynamic_rnn(cell_fw=encoDecoCellFw, cell_bw=encoDecoCellFw)
+            
+            return encoDecoCell
+
         encoDecoCell = tf.contrib.rnn.MultiRNNCell(
             [create_rnn_cell() for _ in range(self.args.numLayers)],
+
+#        encoDecoCell = tf.contrib.rnn.MultiRNNCell(
+#            [create_bi_rnn_cell() for _ in range(self.args.numLayers)],
         )
 
         # Network input (placeholders)
@@ -173,6 +198,7 @@ class Model:
         # Define the network
         # Here we use an embedding model, it takes integer as input and convert them into word vector for
         # better word representation
+        
 #        decoderOutputs, states = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
         decoderOutputs, states = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
             self.encoderInputs,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
